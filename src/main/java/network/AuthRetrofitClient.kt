a8740851a77a73202263392c41aaefa42b980a96
Alsaril <yakovlev1996@gmail.com>
2017-08-24 20:24:05 +0300

    modifier fix

--- src/main/java/network/AuthRetrofitClient.kt
+++ src/main/java/network/AuthRetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AuthRetrofitClient {
-    private val apiService by lazy {
+    val apiService by lazy {
         Retrofit.Builder()
                 .baseUrl("http://188.225.87.129")
                 .addConverterFactory(GsonConverterFactory.create())
